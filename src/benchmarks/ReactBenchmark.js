fe942ab024b5b86ee899283cc3b28b8fde904883
Swizec Teller <swizec@swizec.com>
2018-02-23 00:54:26 -0800

    Add my results for React

--- src/benchmarks/ReactBenchmark.js
+++ src/benchmarks/ReactBenchmark.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 
-const N = 500;
+const N = 1000;
 
 class ReactBenchmark extends Component {
     beforeTime = 0;
     afterTime = 0;
+    times = [];
 
     state = {
         nodes: []
@@ -31,14 +32,21 @@ class ReactBenchmark extends Component {
     after() {
         this.afterTime = new Date();
 
-        this.refs.time.innerHTML = `<code>${this.afterTime -
-            this.beforeTime}ms</code>`;
+        const delta = this.afterTime - this.beforeTime;
+
+        this.refs.time.innerHTML = `<code>${delta}ms</code>`;
+        this.times.push(delta);
     }
 
     get newNodes() {
         return new Array(N)
             .fill()
             .map(() => `${Math.random()}:${new Date().getTime()}`);
+    }
+    get averageTime() {
+        return Math.round(
+            this.times.reduce((m, n) => m + n, 0) / this.times.length
+        );
     }
 
     prepend = () =>
@@ -90,6 +98,8 @@ class ReactBenchmark extends Component {
                 </p>
                 <p>
                     Time to render: <b ref="time" />
+                    <br />
+                    Average time: <code>{this.averageTime}ms</code>
                 </p>
                 <div className="row">
                     <button onClick={this.prepend} className="pulse">
@@ -102,7 +112,7 @@ class ReactBenchmark extends Component {
                         Append {N} nodes
                     </button>
                     <button onClick={this.drop} className="pulse">
-                        Remove {nodes.length} nodes
+                        Drop {nodes.length} nodes
                     </button>
                     <button onClick={this.remove} className="pulse">
                         Remove 1 node
@@ -111,6 +121,22 @@ class ReactBenchmark extends Component {
                 <div className="row benchmark-scratchpad">
                     {nodes.map((k, i) => <div key={k}>{k}</div>)}
                 </div>
+                <h3>My results</h3>
+                <p>Each test repeated 3 times on a prod build of React.</p>
+                <p>
+                    Avg of Avg time to prepend 1000 nodes up to 30,000:{" "}
+                    <code>21ms</code>
+                    <br />
+                    Avg of Avg time to insert 1000 nodes up to 30,000:{" "}
+                    <code>18ms</code>
+                    <br />
+                    Avg of Avg time to append 1000 nodes up to 30,000:{" "}
+                    <code>15ms</code>
+                    <br />
+                    Avg time to drop 30,000 nodes: <code>195ms</code>
+                    <br />
+                    Avg time to remove 1 node from 0,000: <code>27ms</code>
+                </p>
             </div>
         );
     }
